b2d57af6c6d431864c8b6225c5fb0072560f951f
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-07-18 14:45:02 +0400

    The Git repository contains a React application that fetches and displays weather data for a given city using the OpenWeatherMap API. The application allows users to input a city name and retrieve real-time weather information, including the temperature, cloud conditions, and a brief weather description.

---
+++ src/components/Main.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Button, Container, Card, Input, CardHeader, CardBody, Heading, Stack, Box, Text, Spinner } from '@chakra-ui/react';
+
+const Main = () => {
+  const [value, setValue] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [temp, setTemp] = useState('');
+  const [descp,setDescp]= useState("")
+  const [cloud,setCloud] = useState("")
+  const API_KEY = "0a486f8d82e287a035cf7b02398a52f6";
+  const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const kelvinConvertCelsius = 273;
+  
+
+  const handleChangeCity = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setCityName(value);
+  };
+
+  useEffect(() => {
+    if (cityName) {
+      axios.get(`${BASE_URL}?q=${cityName}&appid=${API_KEY}`)
+        .then(response => {
+          setWeatherData(response.data);
+          console.log(response.data);
+          setTemp(Math.floor(response.data.main.temp - kelvinConvertCelsius));
+         
+          setCloud(response.data.weather[0].main)
+          setDescp(response.data.weather[0].description.toString())
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+    setValue("")
+    setCloud("")
+    setDescp("")
+    setTemp("")
+  }, [cityName]);
+
+  return (
+    <Container marginTop={"50px"} display={"flex"} flexDir={"column"} gap={"10px"}>
+      <Input placeholder='Basic usage' value={value} onChange={handleChangeCity} />
+      <Button onClick={handleClick} colorScheme='blue'>Button</Button>
+      {weatherData && (
+        <Card textAlign={"center"} bgImg="url('public/sky-background-video-conferencing_23-2148639325.avif')" bgPosition="center" bgRepeat="no-repeat" bgSize={"cover"}>
+          <CardHeader>
+            <Heading size='md'>{cityName}</Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack spacing='4'>
+              <Box>
+                <Heading size='xs' textTransform='uppercase'>
+                  {temp}°C
+                </Heading>
+                {/* Display other weather data here */}
+              </Box>
+              <Box >
+              <Heading size='xs' textTransform='uppercase'>
+                  {cloud}
+                </Heading>
+                <Text pt='2' fontSize='sm'>
+                  {descp.toUpperCase()}
+                </Text>
+                
+                
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+      )}
+    </Container>
+  );
+};
+
+export default Main;
